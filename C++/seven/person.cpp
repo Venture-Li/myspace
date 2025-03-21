bea6ba4d9da804b1d72f6c00194ba996287ac8a3
wenchao <liwenchaobolt@163.com>
2025-03-21 15:43:04 +0800

    25年3月21日 C++学习

---
+++ C++/seven/person.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+struct Person
+{
+    string name;
+    string address;
+    string print_name() const {return name;};
+    string print_address() const {return address;}
+};
+
+
+int main()
+{
+
+
+
+    return 0;
+}
